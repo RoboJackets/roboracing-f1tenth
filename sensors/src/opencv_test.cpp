7721ef9d98a03e6abbacc3faf7a719471e325c61
justinpgardiner <justinpgardiner@gmail.com>
2025-02-05 23:10:00 +0000

    final attempt to fix encodings before testing

--- sensors/src/opencv_test.cpp
+++ sensors/src/opencv_test.cpp
@@ -31,26 +31,32 @@ private:
     void color_callback(const sensor_msgs::msg::Image::ConstSharedPtr & msg)
     {
         // converting sensor msg image to an OpenCV image
-        auto orig_img = cv_bridge::toCvCopy(msg, "bgr8");
+        auto bridge = cv_bridge::toCvCopy(msg, "bgr8");
+        cv::Mat img;
         // convert to hsv
-        cv::cvtColor(orig_img->image, orig_img->image, cv::COLOR_BGR2HSV);
+        cv::cvtColor(bridge->image, img, cv::COLOR_BGR2HSV);
         // creating a new matrix to fill with filtered values
-        cv::Mat orange_scale_img(orig_img->image.size(), CV_8UC1);
+        cv::Mat orange_scale_img(img.size(), CV_8UC1);
         // loop through rows and columns
-        for (auto r = 0; r < orig_img->image.rows; r++)
+        for (auto r = 0; r < img.rows; r++)
         {
-            for (auto c = 0; c < orig_img->image.cols; c++)
+            for (auto c = 0; c < img.cols; c++)
             {
-                orig_img->image.at<std::uint8_t>(r, c) = hsv_to_gray_orange(orig_img->image.at<cv::Vec3b>(r, c));
+                orange_scale_img.at<std::uint8_t>(r, c) = hsv_to_gray_orange(img.at<cv::Vec3b>(r, c));
             }
         }
-        cv::Mat thresholded_img(orig_img->image.size(), CV_8UC1);
+        // TO DO: Test
+        cv::Mat thresholded_img(orange_scale_img.size(), CV_8UC1);
         // adaptive threshold takes into account changes to color thresholds based on lighting
         cv::adaptiveThreshold(orange_scale_img, thresholded_img, 255,
             cv::THRESH_BINARY, cv::ADAPTIVE_THRESH_MEAN_C, 11, 2);
+        std::cout << "publishing" << std::endl;
+        // update content of bridge with updated image and corresponsing encoding
+        bridge->encoding = "MONO8"; // might actually be 8UC1
+        bridge->image = thresholded_img;
         // from our CVImage, we call toImageMsg() to get the underlying sensor_msgs::ImagePtr
         // Then we must dereference this pointer to access the underlying sensor_msgs::msg::Image
-        publisher_->publish(*orig_img->toImageMsg().get());
+        publisher_->publish(*bridge->toImageMsg().get());
     }
 
     // convert hsv to greyscale with orange weighting
