bb7e3004de0e58c01a2fda9cc7d6eedc9d779628
Justin Gardiner <justinpgardiner@gmail.com>
2025-02-09 18:39:34 -0500

    add basic udp scripting

--- sensors/src/udp_send_message.cpp
+++ sensors/src/udp_send_message.cpp
@@ -2,13 +2,19 @@
 #include <rclcpp/rclcpp.hpp>
 #include <chrono>
 #include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
 
 using namespace std::chrono_literals;
 
 namespace sensors
 {
-    class sendUDPMessage : public rclcpp::Node
-
-}
+class sendUDPMessage : public rclcpp::Node
+{
 public:
-   explicit sendUDPMessage 
+    explicit sendUDPMessage(const rclcpp::NodeOptions & options) : rclcpp::Node("udp_node", options)
+    {
+        
+    }
+}
+}
