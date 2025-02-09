54f723e6c72cbe9c0f75d36aa926945f64c41cb2
Rohan Nair <rohanmanunair@gmail.com>
2025-02-09 16:17:51 -0500

    commited basic UDP messages

---
+++ sensors/src/udp_send_message.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <rclcpp/rclcpp.hpp>
+#include <chrono>
+#include <iostream>
+
+using namespace std::chrono_literals;
+
+namespace sensors
+{
+    class sendUDPMessage : public rclcpp::Node
+
+}
+public:
+   explicit sendUDPMessage 
