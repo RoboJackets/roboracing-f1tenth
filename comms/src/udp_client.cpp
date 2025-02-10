a7feabe9459f6e7525c9ccb1f8c2bf4603e70f9b
justinpgardiner <justinpgardiner@gmail.com>
2025-02-10 21:14:58 +0000

    add comms package and building udp_client class

---
+++ comms/src/udp_client.cpp
@@ -0,0 +1,44 @@
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/array.hpp>
+#include <iostream>
+
+using boost::asio::ip::udp;
+
+class UDPClient
+{
+public:
+    boost::asio::io_service io_service;
+    udp::socket socket;
+    udp::endpoint receiver_endpoint;
+    boost::array<char, 1024> recv_buffer;
+
+    UDPClient();
+    void do_receive();
+    void handle_receive(const boost::system::error_code& error, size_t);
+};
+
+UDPClient::UDPClient()
+    : io_service(),
+      socket(io_service, {udp::v4(), 8888})
+{
+    do_receive();
+    io_service.run();
+}
+
+void UDPClient::do_receive()
+{
+    socket.async_receive_from(boost::asio::buffer(recv_buffer), receiver_endpoint,
+                               boost::bind(&UDPClient::handle_receive, this,
+                               boost::asio::placeholders::error,
+                               boost::asio::placeholders::bytes_transferred));
+}
+
+void UDPClient::handle_receive(const boost::system::error_code& error, size_t bytes_transferred)
+{
+    std::cout << "ulala" << std::endl;
+    std::cout << "Received: '" << std::string(recv_buffer.begin(), recv_buffer.begin()+bytes_transferred) << "'\n";
+
+    if (!error || error == boost::asio::error::message_size)
+        do_receive();
+}
