7b2b778e59cc02233ec896828c7782c6aa58c931
Justin Gardiner <justinpgardiner@gmail.com>
2025-03-09 18:37:38 -0400

    added ros node for udp connection

--- comms/src/udp_client.cpp
+++ comms/src/udp_client.cpp
@@ -2,71 +2,90 @@
 #include <boost/bind/bind.hpp>
 #include <boost/array.hpp>
 #include <iostream>
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
+#include <std_msgs/msg/string.hpp>
+
+using namespace std::chrono_literals;
 
 using boost::asio::ip::udp;
-
+namespace comms
+{
 class UDPClient : public rclcpp::Node
 {
 public:
-   explicit UDPClient(const rclcpp::NodeOptions& options) : Node("udp_client", options)
+   explicit UDPClient(const rclcpp::NodeOptions& options) : Node("udp_client", options), io_service(), socket(io_service, {udp::v4(), 8888})
    {
-    subscription = this->create_subscription<std_msgs::msg::String>("udp_send", 10, std:bind()) -> {
-
-    })
+    io_service.run();
+    subscription_ = this->create_subscription<std_msgs::msg::String>("/planning/desired_chassis_state", 10, std::bind(&UDPClient::udp_callback, this, std::placeholders::_1));
+    timer_ =  this->create_wall_timer(5s, std::bind(&UDPClient::udp_timer_callback, this));
+    do_receive();
+   }
+private:
+    const std::String JETSON_IP = "192.168.20.3";
+    int count = 0;
     boost::asio::io_service io_service;
     udp::socket socket;
     udp::endpoint receiver_endpoint;
     boost::array<char, 1024> recv_buffer;
-    void do_receive();
-    void handle_receive(const boost::system::error_code& error, size_t);
-    void do_send(const std::string& message, const std::string& destination_ip, const unsigned short port);
-   }
-};
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 
-UDPClient::UDPClient()
-    : io_service(),
-      socket(io_service, {udp::v4(), 8888})
-{
-    do_receive();
-    io_service.run();
-}
+    void udp_callback(const std_msgs::msg::String & msg) {
+        if (count % 2 == 0) {
+            this->do_send("V=2.0", JETSON_IP, 8888);
+        } else {
+            this->do_send("V=0.0", JETSON_IP, 8888);
+        }
+    }
 
-void UDPClient::do_receive()
-{
-    socket.async_receive_from(boost::asio::buffer(recv_buffer), receiver_endpoint,
-                               boost::bind(&UDPClient::handle_receive, this,
-                               boost::asio::placeholders::error,
-                               boost::asio::placeholders::bytes_transferred));
-}
+    void udp_timer_callback() {
+        if (count % 2 == 0) {
+            this->do_send("V=2.0", JETSON_IP, 8888);
+        } else {
+            this->do_send("V=2.0", JETSON_IP, 8888);
+        }
+        count++;
+    }
 
-void UDPClient::handle_receive(const boost::system::error_code& error, size_t bytes_transferred)
-{
-    std::cout << "ulala" << std::endl;
-    std::cout << "Received: '" << std::string(recv_buffer.begin(), recv_buffer.begin()+bytes_transferred) << "'\n";
+    void do_send(const std::string& message, const std::string& destination_ip, const unsigned short port) {
+        std::cout << "method called!" << std::endl;
+        auto remote = boost::asio::ip::udp::endpoint(boost::asio::ip::address::from_string(destination_ip), port);
+        try {
+            std::cout << "Sent!" << std::endl;
+            socket.send_to(boost::asio::buffer(message), remote);
+
+        } catch (const boost::system::system_error& ex) {
+            std::cout << "Not Sent!"/*things need to go here*/ << std::endl;
+        }
+    }
+
+    void do_receive()
+    {
+        socket.async_receive_from(boost::asio::buffer(recv_buffer), receiver_endpoint,
+                                boost::bind(&UDPClient::handle_receive, this,
+                                boost::asio::placeholders::error,
+                                boost::asio::placeholders::bytes_transferred));
+    }
+    void handle_receive(const boost::system::error_code& error, size_t bytes_transferred)
+    {
+        std::cout << "Received: '" << std::string(recv_buffer.begin(), recv_buffer.begin()+bytes_transferred) << "'\n";
 
     if (!error || error == boost::asio::error::message_size)
         do_receive();
-}
-void udpcallback()
-
-void UDPClient::do_send(const std::string& message, const std::string& destination_ip, const unsigned short port) {
-	auto remote = boost::asio::ip::udp::endpoint(boost::asio::ip::address::from_string(destination_ip), port);
-	try {
-		socket.send_to(boost::asio::buffer(message), remote);
-
-	} catch (const boost::system::system_error& ex) {
-		std::cout << "Not Sent!" << std::endl;
-	}
-}
-
-int main(int argc, char **argv)
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<UDPClient>());
+    }
     
-    UDPClient client;
-    client.do_send("hello world", "192.168.20.2", 8888);
-    rclcpp::shutdown();
-    return 0;
+
+};
+    RCLCPP_COMPONENTS_REGISTER_NODE(comms::UDPClient)
 }
+
+
+
+
+
+
+
+
+
+
